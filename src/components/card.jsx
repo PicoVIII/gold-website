9e81cc652cf54edb9fa4feaa1cdf2b573c1ea96b
unknown <judevalen.suson@gmail.com>
2025-08-07 00:53:11 +0800

    Improved product card design

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,16 +1,29 @@
 import React from "react";
 import "./card.css";
 
+function getWearLevel(float) {
+    if (float <= 0.07) return "FN";
+    if (float <= 0.15) return "MW";
+    if (float <= 0.38) return "FT";
+    if (float <= 0.45) return "WW";
+    if (float <= 0.70) return "BS";
+    return "Unknown";
+}
+function getStattrak(stattrak){
+    return stattrak ? "ST" : " ";
+}
 function Card({ product }) {
     return (
         <div className="products-card">
             <img src={require(`./product_image/${product.image}`)} alt={product.skin} />
+            <div className="float-line">
+                <p>{getWearLevel(product.float)} / {product.float.toFixed(8)}</p>
+                <p>{getStattrak(product.stattrak)}</p>
+            </div>
             <hr className={`rarity-line ${product.rarity.toLowerCase()}`} />
             <h3>{product.gun} - {product.skin}</h3>
-            <p>Float: {product.float}</p>
-            <p>Wear Level: {product.wearLevel}</p>
             <p>Pattern: {product.pattern}</p>
-            <p>Price: {product.price}</p>
+            <p>Price: ${product.price}</p>
             <button>Add to Cart</button>
         </div>
     );
--- src/content.jsx
+++ src/content.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-/*import wallpaper from './assets/cswallpaper.svg'; */
+import openingsale from './assets/openingsale.png'; // Importing the promotional image
 import './content.css'; // Importing the CSS for the content
 const promo = {
     title: "Opening Sale",
     description: "Up to 40% off on your first purchase!",
-}
+    image: openingsale
+};
 function Content() {
     return (
-        <section className='hero-section'>
+        <section className='hero-section' style={{ backgroundImage: `url(${promo.image})` }}>
             <div className='hero-content'>
                 <div className='hero-text'>
                     <h1>{promo.title}</h1>
